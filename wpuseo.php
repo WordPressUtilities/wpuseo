d917803da6fb949289cd83e62d79e8bdd45671cd
Kevin <kevin@rocher-dev.com>
2017-07-18 23:06:13 +0200

    v 1.22.0
    * Analytics : Add dimension logged in.
    * Analytics : Add dimension user ID.

--- wpuseo.php
+++ wpuseo.php
@@ -4,7 +4,7 @@
 Plugin Name: WPU SEO
 Plugin URI: https://github.com/WordPressUtilities/wpuseo
 Description: Enhance SEO : Clean title, nice metas.
-Version: 1.21.0
+Version: 1.22.0
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -1232,8 +1232,16 @@ class WPUSEO {
 
         echo '<script type="text/javascript">';
         echo "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');";
-        echo "ga('create', '" . $ua_analytics . "', 'auto');";
-        echo "ga('send', 'pageview');";
+        echo "\nga('create','" . $ua_analytics . "','auto');";
+        if (is_user_logged_in() && apply_filters('wpuseo_analytics_track_user_id', true)) {
+            $user = wp_get_current_user();
+            if (is_object($user)) {
+                echo "\nga('set','userId','" . $user->ID . "');";
+            }
+        }
+        echo "\nga('set','dimension1','visitorloggedin-" . (is_user_logged_in() ? '1' : '0') . "');";
+        echo "\nga('send','pageview');";
+
         if (!empty($hook_ajaxready)) {
             echo "function wpuseo_callback_ajaxready(){ga('send','pageview');}";
             echo "if (typeof(jQuery) == 'undefined') {document.addEventListener('" . esc_attr($hook_ajaxready) . "',wpuseo_callback_ajaxready,1);}";
