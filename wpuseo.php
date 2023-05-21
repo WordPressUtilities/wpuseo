c58def4769af3987887ee726abc9a1fdbe469093
Kevin <kevin@rocher-dev.com>
2023-05-21 21:27:19 +0200

    v 2.21.2
    - Add a github workflow to PHPLint files.

--- wpuseo.php
+++ wpuseo.php
@@ -5,7 +5,7 @@ Plugin Name: WPU SEO
 Plugin URI: https://github.com/WordPressUtilities/wpuseo
 Update URI: https://github.com/WordPressUtilities/wpuseo
 Description: Enhance SEO : Clean title, Nice metas, GDPR friendly Analytics.
-Version: 2.21.1
+Version: 2.21.2
 Author: Darklg
 Author URI: https://darklg.me/
 Text Domain: wpuseo
@@ -19,7 +19,7 @@ Contributors: @boiteaweb, @CecileBr
 
 class WPUSEO {
 
-    public $plugin_version = '2.21.1';
+    public $plugin_version = '2.21.2';
     private $active_wp_title = true;
     private $active_metas = true;
     private $fake_txt_files = array('ads', 'robots');
