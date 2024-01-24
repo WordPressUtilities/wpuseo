1ea96230689e470f0d7d0807dd4c9d4ee88bb55e
Kevin <kevin@rocher-dev.com>
2024-01-24 21:50:26 +0100

    v 2.23.1
    - Ignore .mo.php files.
    - Version number bump to acknowledge the fix from @CecileBr.

--- wpuseo.php
+++ wpuseo.php
@@ -5,7 +5,7 @@ Plugin Name: WPU SEO
 Plugin URI: https://github.com/WordPressUtilities/wpuseo
 Update URI: https://github.com/WordPressUtilities/wpuseo
 Description: Enhance SEO : Clean title, Nice metas, GDPR friendly Analytics.
-Version: 2.23.0
+Version: 2.23.1
 Author: Darklg
 Author URI: https://darklg.me/
 Text Domain: wpuseo
@@ -19,7 +19,7 @@ Contributors: @boiteaweb, @CecileBr
 
 class WPUSEO {
 
-    public $plugin_version = '2.23.0';
+    public $plugin_version = '2.23.1';
     private $active_wp_title = true;
     private $active_metas = true;
     private $fake_txt_files = array('ads', 'robots');
